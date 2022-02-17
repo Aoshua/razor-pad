88733a3de0e5c81f992a81de39b72eae7a7924f9
aoshua <64.josh@gmail.com>
2022-02-17 10:34:14 -0700

    the story so far...

--- razor.py
+++ razor.py
@@ -1,14 +1,21 @@
-import re
+from cmath import exp
+from fileinput import close
 from tkinter import *
-import tkinter
-from tkinter.ttk import *
+# from tkinter.ttk import *
 from datetime import datetime
 from tkinter  import messagebox
 from tkinter import filedialog,simpledialog
 from tkinter.scrolledtext import ScrolledText
-import pyglet, os
+from turtle import bgcolor
+import pyglet
 
+# Fonts:
 pyglet.font.add_file('fonts/FiraCode-Retina.ttf')
+
+# Colors:
+mainColor = "#373737"
+secondColor = "#272727"
+textColor = "#fff"
 
 # Darkmode:
 # def darkTheme(root):
@@ -23,10 +30,40 @@ pyglet.font.add_file('fonts/FiraCode-Retina.ttf')
 root = Tk()
 root.title('Razor Pad')
 root.resizable(True, True)
+root.overrideredirect(True) # Hides tkinter default titlebar
 # style = darkTheme(root)
 
+def moveApp(e):
+    root.geometry(f'+{e.x_root}+{e.y_root}')
+
+def quitter(e):
+    root.quit()
+
+# Create custom title bar:
+titleBar = Frame(root, bg=secondColor, relief="raised")
+titleBar.pack(expand=1, fill=X, padx=8, pady=4)
+# titleLabel = Label(titleBar, text="RazorPad", bg=secondColor, fg=textColor)
+# titleLabel.pack(side=LEFT, pady=4)
+titleBar.bind("<B1-Motion>", moveApp)
+
+closeIco = PhotoImage(file="img/icons8-close-50.png")
+closeBtn = Button(titleBar, image=closeIco, bg=secondColor, width=12, height=12, bd=0)
+closeBtn.pack(side=RIGHT, pady=4, padx=4)
+closeBtn.bind("<Button-1>", quitter)
+
+collapseIco = PhotoImage(file="img/icons8-collapse-30.png")
+collapseBtn = Button(titleBar, image=collapseIco, bg=secondColor, width=12, height=12, bd=0)
+collapseBtn.pack(side=RIGHT, pady=4, padx=10)
+# collapseBtn.bind("<Button-1>", quitter)
+
+hideIco = PhotoImage(file="img/icons8-minus-24.png")
+hideBtn = Button(titleBar, image=hideIco, bg=secondColor, width=12, height=12, bd=0)
+hideBtn.pack(side=RIGHT, pady=4, padx=10)
+# hideBtn.bind("<Button-1>", quitter)
+
 # Scrollable notepad window
-notepad = ScrolledText(root, width = 90, height = 40, font="FiraCode-Retina")
+notepad = ScrolledText(root, width=90, height=40, font=("FiraCode-Retina", 12))
+notepad.vbar.config(troughcolor=mainColor, bg="blue")
 fileName = ''
 
 # Functions for commands:
@@ -105,49 +142,47 @@ def cmdTimeDate():     # Edit menu Time/Date option
     dtString = now.strftime("%d/%m/%Y %H:%M:%S")
     label = messagebox.showinfo("Time/Date", dtString)
 
-# Notepad menu items:
-notepadMenu = Menu(root)
-root.configure(menu=notepadMenu)
+# # Notepad menu items:
+# notepadMenu = Menu(root)
+# root.configure(menu=notepadMenu)
 
-# File menu:
-fileMenu = Menu(notepadMenu, tearoff = False)
-notepadMenu.add_cascade(label='File', menu = fileMenu)
+# # File menu:
+# fileMenu = Menu(notepadMenu, tearoff = False)
+# notepadMenu.add_cascade(label='File', menu = fileMenu)
 
-# Adding options in file menu:
-fileMenu.add_command(label='New', command = cmdNew)
-fileMenu.add_command(label='Open...', command = cmdOpen)
-fileMenu.add_command(label='Save', command = cmdSave)
-fileMenu.add_command(label='Save As...', command = cmdSaveAs)
-fileMenu.add_separator()
-fileMenu.add_command(label='Exit', command = cmdExit)
+# # Adding options in file menu:
+# fileMenu.add_command(label='New', command = cmdNew)
+# fileMenu.add_command(label='Open...', command = cmdOpen)
+# fileMenu.add_command(label='Save', command = cmdSave)
+# fileMenu.add_command(label='Save As...', command = cmdSaveAs)
+# fileMenu.add_separator()
+# fileMenu.add_command(label='Exit', command = cmdExit)
 
-# Edit menu:
-editMenu = Menu(notepadMenu, tearoff = False)
-notepadMenu.add_cascade(label='Edit', menu = editMenu)
+# # Edit menu:
+# editMenu = Menu(notepadMenu, tearoff = False)
+# notepadMenu.add_cascade(label='Edit', menu = editMenu)
 
-# Edit menu options
-editMenu.add_command(label='Cut', command = cmdCut)
-editMenu.add_command(label='Copy', command = cmdCopy)
-editMenu.add_command(label='Paste', command = cmdPaste)
-editMenu.add_command(label='Delete', command = cmdClear)
-editMenu.add_separator()
-editMenu.add_command(label='Find...', command = cmdFind)
-editMenu.add_separator()
-editMenu.add_command(label='Select All', command = cmdSelectAll)
-editMenu.add_command(label='Time/Date', command = cmdTimeDate)
+# # Edit menu options
+# editMenu.add_command(label='Cut', command = cmdCut)
+# editMenu.add_command(label='Copy', command = cmdCopy)
+# editMenu.add_command(label='Paste', command = cmdPaste)
+# editMenu.add_command(label='Delete', command = cmdClear)
+# editMenu.add_separator()
+# editMenu.add_command(label='Find...', command = cmdFind)
+# editMenu.add_separator()
+# editMenu.add_command(label='Select All', command = cmdSelectAll)
+# editMenu.add_command(label='Time/Date', command = cmdTimeDate)
 
 # Darkmode
 #isDarkMode = False
 def toggle_darkmode():
     # isDarkMode = not isDarkMode
-    mainColor = "#373737"
-    secondColor = "#272727"
-    textColor = "#fff"
     root.config(bg=secondColor)
-    notepadMenu.config(bg=secondColor, fg=textColor)
-    editMenu.config(bg=mainColor, fg=textColor)
-    fileMenu.config(bg=mainColor, fg=textColor)
+    # notepadMenu.config(bg=secondColor, fg=textColor)
+    # editMenu.config(bg=mainColor, fg=textColor)
+    # fileMenu.config(bg=mainColor, fg=textColor)
     notepad.config(bg=mainColor, fg=textColor)
+
 
 toggle_darkmode()
 
